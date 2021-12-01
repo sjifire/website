aefd981149f43013a5119cd454dde4956e8787bd
Steven Bailey <stevenbailey@Stevens-MacBook-Air.local>
2021-12-01 06:45:01 -0700

    split post types

--- .eleventy.js
+++ .eleventy.js
@@ -19,6 +19,12 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.setDataDeepMerge(true);
   
+  eleventyConfig.addFilter("pluck", function (arr, selections, attr) {
+    // Assumes this is receiving a collection, hence the `data`
+    // If custom array such as from _data, update accordingly
+    return arr.filter((item) => selections.includes(item.data[attr]));
+  });
+
   const MarkdownIt = require("markdown-it");
   const mdRender = new MarkdownIt();
   eleventyConfig.addFilter("markdownify", function(rawString) {
