fdd4cf344fc0ff4e7fb1a6f5c13ca2fbbaef2a2a
Steven Bailey <stevenbailey@Stevens-MacBook-Air.local>
2021-12-01 18:50:02 -0700

    cloudinary1

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "San Juan Island Fire and Rescue",
-  rootUrl: "https://sjifire.netlify.app/",
+  rootUrl: "https://sjifire.netlify.app",
   "buildTime" : new Date(),
   env: process.env.ELEVENTY_ENV,
 };
