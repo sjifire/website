7c71967c68fe1d3df962fc3aeb1d0a85f6631698
Steven Bailey <stevenbailey@Stevens-MacBook-Air.local>
2021-12-06 22:48:33 -0700

    meetings checkpoint

--- .eleventy.js
+++ .eleventy.js
@@ -20,14 +20,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setDataDeepMerge(true);
   
   eleventyConfig.addFilter("pluck", function (arr, selections, attr) {
-    // Assumes this is receiving a collection, hence the `data`
-    // If custom array such as from _data, update accordingly
     return arr.filter((item) => selections.includes(item[attr]));
   });
 
   eleventyConfig.addFilter("exclude", function (arr, selections, attr) {
-    // Assumes this is receiving a collection, hence the `data`
-    // If custom array such as from _data, update accordingly
     return arr.filter((item) => !selections.includes(item[attr]));
   });
 
