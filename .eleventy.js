be5c13c3f2eff65d91c07d3b74fc08ff9cde943d
Adam Greene <adam.greene@gmail.com>
2021-12-08 12:25:01 -0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -87,23 +87,23 @@ module.exports = function (eleventyConfig) {
   });
 
   // Minify HTML Output
-  // eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-  //   // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-  //   if( isProduction && outputPath && outputPath.endsWith(".html") ) {
-  //     let minified = htmlmin.minify(content, {
-  //       useShortDoctype: true,
-  //       removeComments: true,
-  //       collapseWhitespace: true
-  //     });
-  //     return minified;
-  //   }
-  //   return content;
-  // });
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    // if( isProduction && outputPath && outputPath.endsWith(".html") ) {
+    //   let minified = htmlmin.minify(content, {
+    //     useShortDoctype: true,
+    //     removeComments: true,
+    //     collapseWhitespace: true
+    //   });
+    //   return minified;
+    // }
+    return content;
+  });
 
-  // return {
-  //   dir: {
-  //     input: "src",
-  //     output: "public",
-  //   },
-  // };
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
 };
