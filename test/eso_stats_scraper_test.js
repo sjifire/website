a87b1f0c44e287297c0b43e84b3e2d81808e2fe1
Adam Greene <adam.greene@gmail.com>
2021-12-22 09:24:18 -0800

    refactor scraper and add tests
    #67

---
+++ test/eso_stats_scraper_test.js
@@ -0,0 +1,133 @@
+const assert = require('assert');
+const fs = require('fs');
+const esoScraper = require( "../src/modules/eso_scraper" );
+const sampleCSVPath = `${__dirname}/sample_report.csv`;
+// const sampleJSONPath = `${__dirname}/sample_stats.json`;
+// const sampleJSON = fs.readFileSync('sample_stats.json', "utf8");
+
+describe('parseCSV', function(){
+  let records = null;
+  before(() => {
+    records = esoScraper.parseCSV(sampleCSVPath);
+  });
+  it('should return expected number of records', function(){
+    assert.equal(records.length, 66)
+  });
+  it('should return an array of objects', function(){
+    assert.equal(typeof(records[0]), 'object')
+  });
+  describe("record within array", function(){
+    it('should contain a "Dispatched Date"', function(){
+      assert.deepStrictEqual(records[0]['Dispatched Date'], new Date('2021-01-01T09:48:04.000Z'))
+    });
+    it('should contain a "En Route Date"', function(){
+      assert.deepStrictEqual(records[0]['En Route Date'], new Date('2021-01-01T09:50:04.000Z'))
+    });
+    it('should contain a "Arrival Date"', function(){
+      assert.deepStrictEqual(records[0]['Arrival Date'], new Date('2021-01-01T09:54:22.000Z'))
+    });
+    it('should contain a "Last Unit Cleared Date"', function(){
+      assert.deepStrictEqual(records[0]['Last Unit Cleared Date'], new Date('2021-01-01T10:13:53.000Z'))
+    });
+
+    it('should contain a "EMS Personnel Count"', function(){
+      assert.equal(records[0]['EMS Personnel Count'], 0)
+    });
+    it('should contain a "Other Personnel Count"', function(){
+      assert.equal(records[0]['Other Personnel Count'], 0)
+    });
+    it('should contain a "Suppression Personnel Count"', function(){
+      assert.equal(records[0]['Suppression Personnel Count'], 2)
+    });
+
+    it('should contain a "Incident Type Code"', function(){
+      assert.equal(records[0]['Incident Type Code'], 321)
+    });
+    it('should contain a "Station"', function(){
+      assert.equal(records[0]['Station'], '(31) Headquarters')
+    });
+  });
+});
+
+describe('generateStats', function(){
+  let records = null;
+  let statsJSON = null;
+  // let stubbedStats = null;
+  let parsedStats  = null;
+  before(() => {
+    records = esoScraper.parseCSV(sampleCSVPath);
+    parsedStats = esoScraper.generateStats(records);
+    // let rawdata = fs.readFileSync(sampleJSONPath);
+    // stubbedStats = JSON.parse(rawdata);
+  });
+  it('should return expected keys', function(){
+    let keys = Object.keys(parsedStats);
+    assert.ok(keys.includes('call_stats'));
+    assert.ok(keys.includes('personnel_stats'));
+    assert.ok(keys.includes('region_stats'));
+    assert.ok(keys.includes('time_stats'));
+    assert.ok(keys.includes('updated_at'));
+  });
+  it('should return expected call_stats', function(){
+    let expectedCallStats = {
+          cancelled: 0,
+          daytime_calls: 44,
+          downgraded: 6,
+          fire: 6,
+          medical_rescue: 54,
+          nighttime_calls: 22,
+          other: 0,
+          overlapping_num_calls: 8,
+          total: 66
+        }
+    assert.deepStrictEqual(parsedStats.call_stats, expectedCallStats);
+  });
+  it('should return expected personnel_stats', function(){
+    let expectedPersonnelStats = {
+         "number_per_incident": {
+           "max": 10,
+           "mean": 2.5606060606060606,
+           "min": 2,
+         },
+         "time_on_incident": {
+           "mean": 4611.363636363636,
+           "sum": 304350
+         }
+       }
+    assert.deepStrictEqual(parsedStats.personnel_stats, expectedPersonnelStats);
+  });
+
+  it('should return expected personnel_stats', function(){
+    let expectedRegionStats = {
+          central: {
+            travel: {
+              mean: 334.1363636363636
+            }
+          },
+          north: {
+            travel: {
+              mean: 1142.25
+            }
+          },
+          south: {
+            travel: {
+              mean: 709.0909090909091
+            }
+          }
+       }
+    assert.deepStrictEqual(parsedStats.region_stats, expectedRegionStats);
+  });
+
+  it('should return expected time_stats', function(){
+    let expectedTimeStats = {
+      on_scene: {
+        mean: 3216.939393939394
+      },
+      reaction: {
+        mean: 82.92424242424242
+      }
+    }
+    assert.deepStrictEqual(parsedStats.time_stats, expectedTimeStats);
+  });
+});
+
