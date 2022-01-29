3ee849c8c0464c3cc7e600ffec7caecc294008b0
Adam Greene <adam.greene@gmail.com>
2022-01-29 13:32:04 -0800

    fixing some linting stuff

--- admin/editor-component-frame.js
+++ admin/editor-component-frame.js
@@ -1,35 +1,35 @@
-if(typeof window === "undefined") window = {};
+if (typeof window === 'undefined') window = {}
 
 window.NetlifyCmsEditorComponentFrame = {
- id:"frame",
- label: "Embedded Frame",
- fields:[
-  {
-    name: "link",
-    label: "URL",
-    widget: "string"
+  id: 'frame',
+  label: 'Embedded Frame',
+  fields: [
+    {
+      name: 'link',
+      label: 'URL',
+      widget: 'string'
+    },
+    {
+      name: 'title',
+      label: 'Title',
+      widget: 'string'
+    }],
+  pattern: /\[iframe link="(.+?)" title="(.+?)"\]/mi,
+  fromBlock: function (match) {
+    return {
+      link: match[1],
+      title: match[2]
+    }
   },
-  {
-    name: "title",
-    label: "Title",
-    widget: "string"
-  }],
- pattern: /\[iframe link="(.+?)" title="(.+?)"\]/mi,
- fromBlock: function(match){
-    return{
-       link: match[1],
-       title: match[2]
-    };
- },
- toBlock: ({link, title}) => {
-    markdown = `[iframe link="${link}" title="${title}"]`
-    return DOMPurify.sanitize(markdown);
- },
- toPreview: ({link, title}) => {
-  return `
+  toBlock: ({ link, title }) => {
+    const markdown = `[iframe link="${link}" title="${title}"]`
+    return DOMPurify.sanitize(markdown)
+  },
+  toPreview: ({ link, title }) => {
+    return `
   <figure class="post__media">
     <div class="embed-responsive embed-responsive-16by9">
-      <iframe width='100%' height='640px' title="${title}" src="${{url}}" loading="lazy" frameborder='0' scrolling='no' style="
+      <iframe width='100%' height='640px' title="${title}" src="${{ link }}" loading="lazy" frameborder='0' scrolling='no' style="
       border:none;overflow:hidden' allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
     </div>
     <figcaption>
@@ -37,5 +37,5 @@ window.NetlifyCmsEditorComponentFrame = {
     </figcaption>
   </figure>
    `
- }
+  }
 }
