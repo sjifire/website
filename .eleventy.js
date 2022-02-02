87a38c5b8c1d2c366ad298ec962fbb5c68c2e50c
Adam Greene <adam.greene@gmail.com>
2022-02-02 12:48:43 -0800

    adding filter to process postURLs

--- .eleventy.js
+++ .eleventy.js
@@ -93,6 +93,10 @@ module.exports = function (eleventyConfig) {
     });
   });
 
+  eleventyConfig.addFilter("postURL", function (post) {
+    return `/news/${this.env.filters.htmlDateStringISO(post.date)}-${this.env.filters.slugify(post.title)}`
+  });
+
   //TODO: cleanup, and perhaps handle both string and obj in one method; future work
 
   eleventyConfig.addFilter("postDateTerseNoYearISO", (dateObj) => {
