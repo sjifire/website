c04613108fad0140e91533ea5f2eae5b03b0877f
Adam Greene <adam.greene@gmail.com>
2024-06-06 18:01:58 -0700

    Update .eleventy.js
    take 2

--- .eleventy.js
+++ .eleventy.js
@@ -166,6 +166,7 @@ module.exports = function (eleventyConfig) {
     //HOWEVER, a double // seems to make it hard for Cloudinary to find the src img...
     // so stripping all leading /
     assetPath = assetPath.replace(/^\/+/, "");
+    return `/${assetPath.replace(/^(\/)?optim\//, ""}`;
     if (!cloudinaryCmds) cloudinaryCmds = "f_auto";
     if (isProduction && siteData.enable_cloudinary_rewrites) {
       if (/^(\/)?optim\//.test(assetPath)) {
