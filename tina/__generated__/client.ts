31f1a712ac284fb43f736ad3313c1949064be02e
Adam Greene <adam.greene@gmail.com>
2026-01-08 21:09:23 -0800

    chore: remove tina/__generated__ from repo

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:7071/api/tina/gql', token: 'undefined', queries,  });
-export default client;
-  
